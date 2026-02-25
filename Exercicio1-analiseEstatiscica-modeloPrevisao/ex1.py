a802117c63ce59be4a71b48025af3ea7cb86257e
thiagosilva <thiagoantonio2108@gmail.com>
2026-02-25 20:58:40 -0300

    implementando 10 registros para base de dados com laço de repetição

--- Exercicio1-analiseEstatiscica-modeloPrevisao/ex1.py
+++ Exercicio1-analiseEstatiscica-modeloPrevisao/ex1.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-fatorE = np.random.randint(0, 21, size=10)   # tempo de estudo
-fatorF = np.random.randint(1, 16, size=10)   # dias de falta
+def dadosAluno():
+    fatorE = np.random.randint(0, 21, size=10)   # tempo de estudo
+    fatorF = np.random.randint(0, 16, size=1)   # dias de falta
+    media = np.mean(fatorE)
+    desvio = np.std(fatorE)
 
-media = np.mean(fatorE)
-desvio = np.std(fatorE)
-
-print("Fator E (tempo de estudo):", fatorE)
-print("Fator F (dias de falta):  ", fatorF)
-print(f"\nMédia de tempo investido: {media:.2f} horas")
-print(f"Desvio padrão:            {desvio:.2f} horas")
+    for i in range(0, 11):
+        print(f"\nAluno: {i}")
+        print(f"FatorE (tempo de estudo): {fatorE}")
+        print(f"FatorF (dias de falta):   {fatorF}")
+        print(f"\nMédia de tempo investido: {media:.2f} horas")
+        print(f"Desvio padrão:            {desvio:.2f} horas")
+        print(f"==========================================================\n")
+    
+dadosAluno()
