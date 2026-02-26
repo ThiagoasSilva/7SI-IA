a32edc6162332a6892cdae079e9385816ed5fdf6
thiagosilva <thiagoantonio2108@gmail.com>
2026-02-25 21:14:58 -0300

    implementação de calculo do indice e diagnostico final com erros de repetição

--- Exercicio1-analiseEstatiscica-modeloPrevisao/ex1.py
+++ Exercicio1-analiseEstatiscica-modeloPrevisao/ex1.py
@@ -3,8 +3,18 @@ import numpy as np
 def dadosAluno():
     fatorE = np.random.randint(0, 21, size=10)   # tempo de estudo
     fatorF = np.random.randint(0, 16, size=1)   # dias de falta
+    iap = (fatorE * 0.6) - (fatorF * 0.4) # calculo do indice
     media = np.mean(fatorE)
     desvio = np.std(fatorE)
+
+    # iap = Indice de aproveitamento padrão
+    
+    if(iap.all() >= 7):
+        dgn = "Alta chance de aprovação"
+    elif(iap.all() < 7 and iap.all() >= 5):
+        dgn = "Risco moderado de reprovação"
+    else:
+        dgn = "Intervenção Imediata"
 
     for i in range(0, 11):
         print(f"\nAluno: {i}")
@@ -12,6 +22,8 @@ def dadosAluno():
         print(f"FatorF (dias de falta):   {fatorF}")
         print(f"\nMédia de tempo investido: {media:.2f} horas")
         print(f"Desvio padrão:            {desvio:.2f} horas")
+        print(f"Diagnóstico Final: {dgn}")
         print(f"==========================================================\n")
     
 dadosAluno()
+
